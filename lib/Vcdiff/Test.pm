4e05468a5b693a2832aed82a7f0b3b0d755a92eb
doug <doug@hcsw.org>
2013-06-15 23:57:27 -0400

    improve test output

--- lib/Vcdiff/Test.pm
+++ lib/Vcdiff/Test.pm
@@ -8,8 +8,35 @@ use File::Temp qw/ tempfile /;
 require Test::More;
 
 
+
+
+our $testcases = [
+  ["", "", "both empty"],
+  ["", "asdfasdfasdfasdf", "only adds"],
+  ["asdfasdfasdfasdf", "", "only dels"],
+
+  ["abcdef", "abcdef", "no change"],
+  ["abcdef", "abcDef", "single bit change"],
+
+  ["abcdefghi"x100, "abcdefghi"x51 . "zzzzzzzzzzzzzzzz" . "abcdefghi"x50, "insert small delta"],
+  ["abcdefghi"x51 . "zzzzzzzzzzzzzzzz" . "abcdefghi"x50, "abcdefghi"x100 . "abcdefghi"x51 . "zzzzzzzzzzzzzzzz" . "abcdefghi"x50, "maybe big delta"],
+  ["\x00"x1000000, "\x01"x1000000, "million 0s to million 1s"],
+];
+
+
+
+
+
+## The first two arguments can be strings or references to a strings
+##  * If it's a string, the in-memory API is used
+##  * If it's a reference, the streaming API is used
+## The third argument is a boolean
+##  * true: streaming
+##  * false: in-memory
+## The fourth is description of test
+
 sub verify {
-  my ($source_arg, $target_arg, $output_arg) = @_;
+  my ($source_arg, $target_arg, $streaming_output, $test_name) = @_;
 
   my ($source, $target, $delta);
 
@@ -31,7 +58,7 @@ sub verify {
 
   my ($target2, $target2_fh);
 
-  if ($output_arg) {
+  if ($streaming_output) {
     $delta = tempfile();
     $delta->autoflush(1);
     $target2_fh = tempfile();
@@ -51,46 +78,54 @@ sub verify {
   }
 
   if (ref $target_arg) {
-    Test::More::is($target2, $$target_arg);
+    Test::More::is($target2, $$target_arg, $test_name);
   } else {
-    Test::More::is($target2, $target);
+    Test::More::is($target2, $target, $test_name);
   }
 }
 
 
-my $testcases = [
-  ["abcdef", "abcdef"],
-  ["abcdef", "abcDef"],
-  ["abcdefghi"x100, "abcdefghi"x51 . "zzzzzzzzzzzzzzzz" . "abcdefghi"x50],
-  ["abcdefghi"x51 . "zzzzzzzzzzzzzzzz" . "abcdefghi"x50, "abcdefghi"x100, "abcdefghi"x51 . "zzzzzzzzzzzzzzzz" . "abcdefghi"x50],
-  ["\x00"x1000000, "\x01"x1000000],
-];
 
 
-## Some backends may only support an in memory API so this is split out into its own test routine
+
+
+## Some backends might only support an in memory API so this is split out into its own test routine
 
 sub in_mem {
   foreach my $testcase (@$testcases) {
-    verify($testcase->[0], $testcase->[1]);
+    die "incorrect number of elements in testcase spec" if @$testcase != 3;
+
+    verify($testcase->[0], $testcase->[1], undef, $testcase->[2]);
   }
 }
 
 
-## Try every combination of streaming/in-memory, except for 0 which is the same as the in_mem tests.
+## Try every combination of streaming/in-memory
 
 sub streaming {
   my $opt = shift;
 
   foreach my $testcase (@$testcases) {
-    for my $i (1..7) {
-      my ($t1, $t2, $t3) = ($testcase->[0], $testcase->[1], undef);
-      $t1 = \$t1 if $i & 1;
-      $t2 = \$t2 if $i & 2;
-      $t3 = 1 if $i & 4;
+    die "incorrect number of elements in testcase spec" if @$testcase != 3;
+
+    Test::More::diag($testcase->[2]);
+
+    for my $i (0..7) {
+      my $src = $testcase->[0];
+      my $trg = $testcase->[1];
+      my $streaming_output;
+
+      $src = \$src if $i & 1;
+      $trg = \$trg if $i & 2;
+      $streaming_output = 1 if $i & 4;
 
       next if $opt->{skip_streaming_source_tests} && ($i & 1);
 
-      verify($t1, $t2, $t3);
+      my $combination = (($i & 1) ? 'M' : 'S') .
+                        (($i & 2) ? 'M' : 'S') .
+                        (($i & 4) ? 'S' : 'M');
+
+      verify($src, $trg, $streaming_output, "[$combination]");
     }
   }
 }
